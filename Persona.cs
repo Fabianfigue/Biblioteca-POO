7aa2349bdb962e643daebb0a697f83df2c5bc4cb
Fabianfigue <fabianfigueredo98@outlook.com>
2026-05-21 23:40:00 -0300

    Creacion de persona.cs y usuario.cs

--- Persona.cs
+++ Persona.cs
@@ -1,4 +1,38 @@
-public class Persona
+using System;
+
+namespace BIBLIOTECATP
 {
-    public string nombre;
+    //CLASE PARA CUALQUIER TIPO DE PERSONA
+    public class Persona
+    {
+        //PROPIEDADES
+        private string nombre;
+        private string apellido;
+        private string dni;
+
+        //CONSTRUCTOR
+        public Persona(string nombre, string apellido, string dni)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.dni = dni;
+        }
+
+
+        //PROPIEDADES PUBLICAS
+        public string Nombre
+        {
+            get { return nombre; }
+        }
+
+        public string Apellido
+        {
+            get{ return apellido; }
+        }
+
+        public string DNI
+        {
+            get{ return dni; }
+        }
+    }
 }
---
+++ Usuario.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace BIBLIOTECATP
+{
+    public class Usuario : Persona
+    {
+        //PROPIEDAD DE USUARIO
+        private string telefono;
+
+        //CONSTRUCTOR
+        public Usuario(string nombre, string apellido, string dni, string telefono): base(nombre, apellido, dni)
+        {
+            this.telefono = telefono;
+        }
+
+        public string Telefono
+        {
+            get { return telefono; }
+        }
+    }
+}
