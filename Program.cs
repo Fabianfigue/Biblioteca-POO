a00e4bba80ff7cf74a7ebe3dfb430c039e461a8f
Fabianfigue <fabianfigueredo98@outlook.com>
2026-05-21 22:12:06 -0300

    Inicio de proyecto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BIBLIOTECATP;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Prueba de push");
+        Console.WriteLine("Si lo ves, hiciste bien el pull");
+    }
+}
